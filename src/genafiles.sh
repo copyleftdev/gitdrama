5cb9c0552904ff08886233e5e9cc779b380c357b
Don Johnson <copyleftdev@gmail.com>
2015-04-30 20:00:03 -0700

    changed directory structure

--- src/genafiles.sh
+++
@@ -1,4 +0,0 @@
-for NUM in `seq 1 1 1000`
-do
-  echo "print(test file)" > $NUM-a-test.py
-done
--- src/genscala.sh
+++
@@ -1,7 +0,0 @@
-for NUM in `seq 1 1 1000`
-do
-  touch scala_$NUM.scala
-  echo "object HelloWorld extends App {
-  println("Hello, world!")
-}"  > scala_$NUM.scala
-done
